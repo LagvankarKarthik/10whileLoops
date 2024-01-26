5899bbbf76ded888f94f7407840d6dc080c70329
LagvankarKarthik <77400421+LagvankarKarthik@users.noreply.github.com>
2024-01-26 09:06:24 +0000

    doWhileLoop

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const readlineSync = require('readline-sync');
-let a = readlineSync.question('Enter a number: ');
+const readlineSync = require("readline-sync");
+let a = readlineSync.question("Enter a number: ");
 a = Number.parseInt(a);
 
 let i = 0;
-while (i<a) {
-  console.log('Haha', i);
+while (i < a) {
+  console.log("Haha", i);
   i++;
 }
+
+//Do while loop
+let n = readlineSync.question("Enter a number: ");
+n = Number.parseInt(n);
+
+let j = 0;
+do {
+  console.log("Haha", j);
+  j++;
+} while (j < n);
