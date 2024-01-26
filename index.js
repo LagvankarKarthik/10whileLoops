8f3b510d37cbd1bc2a5d32f56d8f371735b1b2ae
LagvankarKarthik <77400421+LagvankarKarthik@users.noreply.github.com>
2024-01-26 08:42:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const readlineSync = require('readline-sync');
+let a = readlineSync.question('Enter a number: ');
+a = Number.parseInt(a);
+
+let i = 0;
+while (i<a) {
+  console.log('Haha', i);
+  i++;
+}
