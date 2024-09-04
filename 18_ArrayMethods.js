e3f2cb736842db455bce7e96b49273edd8d1689e
Karthik Lagvankar <klagvankar04@gmail.com>
2024-09-04 21:15:14 +0530

    After array methods

--- 18_ArrayMethods.js
+++ 18_ArrayMethods.js
@@ -13,3 +13,28 @@ let arr3 = [false, true, null, undefined, "AI"];
 console.log(arr1.length, arr2.length, arr3.length);
 let concatArray = arr1.concat(arr2, arr3);
 console.log(concatArray, concatArray.length);
+
+const compare = (a, b) => {
+  return a - b;
+};
+
+let ex = [20, 109, 9000009, 109845620];
+ex.sort(compare);
+console.log(ex);
+
+let x = [
+  12,
+  null,
+  undefined,
+  90.67,
+  888,
+  "Dimensity 8020",
+  "iPhone 15 Pro Max",
+];
+console.log(x);
+x.splice(2, 5, true, "Snapdragon 7 gen 3", "Motorola Edge 50 Pro", undefined);
+console.log(x);
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let deletedNumbers = numbers.splice(2, 4, -3, -4, -5, -6);
+console.log(deletedNumbers, numbers);
