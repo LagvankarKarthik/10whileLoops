52d9811e5148bac4b4084feb0dea69f009c96af1
Karthik Lagvankar <klagvankar04@gmail.com>
2024-10-31 10:35:09 +0530

    After local storage

---
+++ 69_localStorage/script.js
@@ -0,0 +1,9 @@
+localStorage.setItem("name", "Karthik");
+
+let key = prompt("Enter the key you want to set: ");
+let value = prompt("Enter the value you want to set: ");
+localStorage.setItem(key, value);
+
+console.log(`The value at ${key} is ${value}.`);
+
+console.log(localStorage.length);
